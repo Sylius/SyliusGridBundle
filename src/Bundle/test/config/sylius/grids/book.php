fdc874ef740d9a807379943a620a1ffb062b1d6c
Loïc Frémont <lc.fremont@gmail.com>
2021-06-02 17:07:31 +0200

    Fix Book grid

--- src/Bundle/test/config/sylius/grids/book.php
+++ src/Bundle/test/config/sylius/grids/book.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Entity\Author;
 use App\Entity\Book;
+use App\Entity\Nationality;
 use Sylius\Component\Grid\Config\Builder\Action;
 use Sylius\Component\Grid\Config\Builder\Field;
 use Sylius\Component\Grid\Config\Builder\Filter;
@@ -9,34 +11,54 @@ use Sylius\Component\Grid\Config\Builder\GridConfig;
 
 return static function (GridConfig $grid) {
     $grid->addGrid(GridBuilder::create('app_book', Book::class)
-        ->addField(Field::create('firstName', 'string')
-            ->setLabel('sylius.ui.first_name')
-            ->setSortable(true)
-        )
-        ->addField(Field::create('lastName', 'string')
-            ->setLabel('sylius.ui.last_name')
-            ->setSortable(true)
-        )
-        ->addOrderBy('lastName')
-        ->addFilter(Filter::create('search', 'string')
-            ->setOptions([
-                'fields' => ['firstName', 'lastName']
+        ->addFilter(Filter::create('title', 'string'))
+        ->addFilter(Filter::create('author', 'entity')
+            ->setFormOptions([
+                'class' => Author::class,
+                'multiple' => true,
             ])
         )
-        ->addCreateAction()
-        ->addItemAction(Action::create('showBooks', 'default')
-            ->setLabel('app.ui.show_books')
-            ->setIcon('book')
+        ->addFilter(Filter::create('nationality', 'entity')
             ->setOptions([
-                'link' => [
-                    'route' => 'app_backend_book_index',
-                    'parameters' => [
-                        'criteria' => ['author' => 'resource.id'],
-                    ],
+                'fields' => ['author.nationality'],
+            ])
+            ->setFormOptions([
+                'class' => Nationality::class,
+            ])
+        )
+        ->addFilter(Filter::create('currencyCode', 'string')
+            ->setOptions([
+                'fields' => ['price.currencyCode'],
+            ])
+        )
+        ->addFilter(Filter::create('state', 'select')
+            ->setFormOptions([
+                'multiple' => true,
+                'choices' => [
+                    'initial' => 'initial',
+                    'published' => 'published',
+                    'unpublished' => 'unpublished',
                 ],
             ])
         )
-        ->addUpdateAction()
-        ->addDeleteAction()
+        ->orderBy('title', 'asc')
+        ->addField(
+            Field::create('title', 'string')
+                ->setLabel('Title')
+                ->setSortable(true)
+        )
+        ->addField(
+            Field::create('author', 'string')
+                ->setLabel('Author')
+                ->setPath('author.name')
+                ->setSortable(true, 'author.name')
+        )
+        ->addField(
+            Field::create('nationality', 'string')
+                ->setLabel('Nationality')
+                ->setPath('author.nationality.name')
+                ->setSortable(true, 'author.nationality.name')
+        )
+        ->setLimits([10, 5, 15])
     );
 };
