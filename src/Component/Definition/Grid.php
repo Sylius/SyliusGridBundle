fcb3fbbac3e9801e8bc53e8500aa5675f8a2e11c
Loïc Frémont <lc.fremont@gmail.com>
2021-11-04 00:15:42 +0100

    Remove changes on grid definition

--- src/Component/Definition/Grid.php
+++ src/Component/Definition/Grid.php
@@ -61,11 +61,6 @@ class Grid
         return $this->driver;
     }
 
-    public function setDriver(string $driver): void
-    {
-        $this->driver = $driver;
-    }
-
     public function getDriverConfiguration(): array
     {
         return $this->driverConfiguration;
