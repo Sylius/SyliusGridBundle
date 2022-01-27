005912389984e23979b7dc3cea210a0707f6a6fd
Loïc Frémont <lc.fremont@gmail.com>
2022-01-27 17:20:08 +0100

    Comment TwigField cause template does not exist yet

--- src/Bundle/Tests/Functional/Maker/MakeGridTest.php
+++ src/Bundle/Tests/Functional/Maker/MakeGridTest.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 
 namespace Sylius\Bundle\GridBundle\Tests\Functional\Maker;
 
+use App\Entity\Book;
 use App\Entity\Price;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\MakerBundle\Exception\RuntimeCommandException;
@@ -20,17 +21,34 @@ use Symfony\Component\Console\Tester\CommandTester;
 
 final class MakeGridTest extends MakerTestCase
 {
+    public const BOOK_GRID_PATH = 'Grid/BookGrid.php';
+    public const PRICE_GRID_PATH = 'Grid/PriceGrid.php';
+    public const INVALID_GRID_PATH = 'Grid/InvalidGrid.php';
+
     /** @test */
     public function it_can_create_grids(): void
     {
         $tester = new CommandTester((new Application(self::bootKernel()))->find('make:grid'));
 
-        $this->assertFileDoesNotExist(self::tempFile('Grid/PriceGrid.php'));
+        $this->assertFileDoesNotExist(self::tempFile(self::PRICE_GRID_PATH));
 
         $tester->execute(['entity' => Price::class, '--namespace' => 'Tests\Tmp\Grid']);
 
-        $this->assertFileExists(self::tempFile('Grid/PriceGrid.php'));
-        $this->assertSame(self::getPriceGridExpectedContent(), \file_get_contents(self::tempFile('Grid/PriceGrid.php')));
+        $this->assertFileExists(self::tempFile(self::PRICE_GRID_PATH));
+        $this->assertSame(self::getPriceGridExpectedContent(), \file_get_contents(self::tempFile(self::PRICE_GRID_PATH)));
+    }
+
+    /** @test */
+    public function it_can_create_grids_with_boolean_fields(): void
+    {
+        $tester = new CommandTester((new Application(self::bootKernel()))->find('make:grid'));
+
+        $this->assertFileDoesNotExist(self::tempFile(self::BOOK_GRID_PATH));
+
+        $tester->execute(['entity' => Book::class, '--namespace' => 'Tests\Tmp\Grid']);
+
+        $this->assertFileExists(self::tempFile(self::BOOK_GRID_PATH));
+        $this->assertSame(self::getBookGridExpectedContent(), \file_get_contents(self::tempFile(self::BOOK_GRID_PATH)));
     }
 
     /** @test */
@@ -38,14 +56,13 @@ final class MakeGridTest extends MakerTestCase
     {
         $tester = new CommandTester((new Application(self::bootKernel()))->find('make:grid'));
 
-        $this->assertFileDoesNotExist(self::tempFile('Grid/PriceGrid.php'));
+        $this->assertFileDoesNotExist(self::tempFile(self::PRICE_GRID_PATH));
 
         $tester->setInputs([Price::class]);
         $tester->execute(['--namespace' => 'Tests\Tmp\Grid']);
 
-        $this->assertFileExists(self::tempFile('Grid/PriceGrid.php'));
-        $this->assertSame(self::getPriceGridExpectedContent(), \file_get_contents(self::tempFile('Grid/PriceGrid.php'))
-        );
+        $this->assertFileExists(self::tempFile(self::PRICE_GRID_PATH));
+        $this->assertSame(self::getPriceGridExpectedContent(), \file_get_contents(self::tempFile('Grid/PriceGrid.php')));
     }
 
     /** @test */
@@ -53,18 +70,117 @@ final class MakeGridTest extends MakerTestCase
     {
         $tester = new CommandTester((new Application(self::bootKernel()))->find('make:grid'));
 
-        $this->assertFileDoesNotExist(self::tempFile('Grid/InvalidGrid.php'));
+        $this->assertFileDoesNotExist(self::tempFile(self::INVALID_GRID_PATH));
 
         try {
             $tester->execute(['entity' => 'Invalid']);
         } catch (RuntimeCommandException $e) {
             $this->assertSame('Entity "Invalid" not found.', $e->getMessage());
-            $this->assertFileDoesNotExist(self::tempFile('Grid/InvalidGrid.php'));
+            $this->assertFileDoesNotExist(self::tempFile(self::INVALID_GRID_PATH));
 
             return;
         }
 
         $this->fail('Exception not thrown.');
+    }
+
+    private static function getBookGridExpectedContent(): string
+    {
+        return <<<EOF
+<?php
+
+namespace App\Tests\Tmp\Grid;
+
+use App\Entity\Book;
+use Sylius\Bundle\GridBundle\Builder\Action\CreateAction;
+use Sylius\Bundle\GridBundle\Builder\Action\DeleteAction;
+use Sylius\Bundle\GridBundle\Builder\Action\ShowAction;
+use Sylius\Bundle\GridBundle\Builder\Action\UpdateAction;
+use Sylius\Bundle\GridBundle\Builder\ActionGroup\BulkActionGroup;
+use Sylius\Bundle\GridBundle\Builder\ActionGroup\ItemActionGroup;
+use Sylius\Bundle\GridBundle\Builder\ActionGroup\MainActionGroup;
+use Sylius\Bundle\GridBundle\Builder\Field\DateTimeField;
+use Sylius\Bundle\GridBundle\Builder\Field\StringField;
+use Sylius\Bundle\GridBundle\Builder\Field\TwigField;
+use Sylius\Bundle\GridBundle\Builder\GridBuilderInterface;
+use Sylius\Bundle\GridBundle\Grid\ResourceAwareGridInterface;
+use Sylius\Bundle\GridBundle\Grid\AbstractGrid;
+
+final class BookGrid extends AbstractGrid implements ResourceAwareGridInterface
+{
+    public function __construct()
+    {
+        // TODO inject services if required
+    }
+
+    public static function getName(): string
+    {
+        return 'app_book';
+    }
+
+    public function buildGrid(GridBuilderInterface \$gridBuilder): void
+    {
+        \$gridBuilder
+            // see https://github.com/Sylius/SyliusGridBundle/blob/master/docs/field_types.md
+            ->addField(
+                StringField::create('title')
+                    ->setLabel('Title')
+                    ->setSortable(true)
+             )
+            ->addField(
+                StringField::create('state')
+                    ->setLabel('State')
+                    ->setSortable(true)
+             )
+             //->addField(
+             //    TwigField::create('enabled', 'path/to/field/template.html.twig')
+             //        ->setLabel('Enabled')
+             //)
+            ->addField(
+                DateTimeField::create('createdAt')
+                    ->setLabel('CreatedAt')
+             )
+            ->addField(
+                DateTimeField::create('updatedAt')
+                    ->setLabel('UpdatedAt')
+             )
+            ->addField(
+                DateTimeField::create('publishedAt')
+                    ->setLabel('PublishedAt')
+             )
+            ->addField(
+                StringField::create('price.currencyCode')
+                    ->setLabel('Price.currencyCode')
+                    ->setSortable(true)
+             )
+            ->addActionGroup(
+                MainActionGroup::create(
+                    CreateAction::create(),
+                )
+            )
+            ->addActionGroup(
+                ItemActionGroup::create(
+                    // ShowAction::create(),
+                    UpdateAction::create(),
+                    DeleteAction::create()
+                )
+            )
+            ->addActionGroup(
+                BulkActionGroup::create(
+                    DeleteAction::create()
+                )
+            )
+        ;
+    }
+
+    public function getResourceClass(): string
+    {
+        return Book::class;
+    }
+}
+
+EOF
+            ;
     }
 
     private static function getPriceGridExpectedContent(): string
