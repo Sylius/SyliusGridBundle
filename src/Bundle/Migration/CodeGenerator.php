68d86e5a72616103887a58d9becfd2200bc205df
mamazu <14860264+mamazu@users.noreply.github.com>
2022-11-06 17:49:22 +0100

    Adding the code generator with tests
    Grid Migration part 1

---
+++ src/Bundle/Migration/CodeGenerator.php
@@ -0,0 +1,183 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\GridBundle\Migration;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\ClassConstFetch;
+use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\Identifier;
+use PhpParser\Node\Name;
+use PhpParser\Node\Param;
+use PhpParser\Node\Scalar\String_;
+use PhpParser\Node\Stmt\Class_;
+use PhpParser\Node\Stmt\ClassMethod;
+use PhpParser\Node\Stmt\Return_;
+use PhpParser\Node\Stmt\Use_;
+use PhpParser\Node\Stmt\UseUse;
+
+final class CodeGenerator
+{
+    /** @var array<string> */
+    private array $classesToUse = [];
+
+    /** @var array<Node> */
+    private array $classNodes = [];
+
+    private ?Node $namespace = null;
+
+    public function __construct(
+        private CodeOutputter $codeOutputter,
+    ) {
+    }
+
+    public function setNamespace(string $namespace): void
+    {
+        $this->namespace = new Node\Stmt\Namespace_(new Name($namespace));
+    }
+
+    public function addUseStatement(string $classToUse): void
+    {
+        $this->classesToUse[] = $classToUse;
+    }
+
+    /**
+     * If you want to use a reference to a class but don't want to use the FQN everywhere you
+     * need an import.
+     * This function adds an import for the class and returns the imported name back
+     *
+     * @param class-string $className
+     */
+    public function getRelativeClassName(string $className): Name
+    {
+        $this->addUseStatement($className);
+
+        $namespaceParts = explode('\\', $className);
+
+        return new Name(end($namespaceParts));
+    }
+
+    public function addClass(string $className, ?string $extends, ?array $implements, array $body): void
+    {
+        $classConfiguration = ['stmts' => $body];
+
+        if ($extends !== null) {
+            $classConfiguration['extends'] = new Identifier($extends);
+        }
+
+        if (count($implements ?? []) > 0) {
+            $classConfiguration['implements'] = array_map(
+                static fn (string $interfaceName) => new Identifier($interfaceName),
+                $implements,
+            );
+        }
+
+        $this->classNodes[] = new Class_(new Identifier($className), $classConfiguration);
+    }
+
+    public function generateGetResourceClassFunction(string $resourceNameOrString): Node
+    {
+        // If it's a class generate something like: Order::class
+        if (class_exists($resourceNameOrString)) {
+            $returnStatement = new ClassConstFetch(
+                $this->getRelativeClassName($resourceNameOrString),
+                'class',
+            );
+        } else {
+            // Otherwise just return the string as is
+            $returnStatement = new String_($resourceNameOrString);
+        }
+
+        return new ClassMethod(
+            new Identifier('getResourceClass'),
+            [
+                'flags' => Class_::MODIFIER_PUBLIC,
+                'returnType' => 'string',
+                'stmts' => [
+                    new Return_($returnStatement),
+                ],
+            ],
+        );
+    }
+
+    public static function createStaticFunction(string $functionName, string $returnValue): Node
+    {
+        return new ClassMethod(
+            new Identifier($functionName),
+            [
+                'flags' => Class_::MODIFIER_PUBLIC | Class_::MODIFIER_STATIC,
+                'returnType' => 'string',
+                'stmts' => [
+                    new Return_(new String_($returnValue)),
+                ],
+            ],
+        );
+    }
+
+    public static function createFunction(
+        string $functionName,
+        array $params,
+        array $body,
+    ): Node {
+        return new ClassMethod(
+            new Identifier($functionName),
+            [
+                'flags' => Class_::MODIFIER_PUBLIC,
+                'returnType' => 'void',
+                'params' => array_map(
+                    fn (array $param) => new Param(new Variable($param[1]), null, $param[0]),
+                    $params,
+                ),
+                'stmts' => $body,
+            ],
+        );
+    }
+
+    public function build(): string
+    {
+        $nodes = [];
+        if ($this->namespace !== null) {
+            $nodes[] = $this->namespace;
+        }
+
+        foreach ($this->getUseStatementNodes() as $useStatements) {
+            $nodes[] = $useStatements;
+        }
+
+        foreach ($this->classNodes as $classNode) {
+            $nodes[] = $classNode;
+        }
+
+        $generatedCode = "<?php\n" . $this->codeOutputter->prettyPrint($nodes);
+
+        $this->classNodes = [];
+        $this->namespace = null;
+        $this->classesToUse = [];
+
+        return $generatedCode;
+    }
+
+    /**
+     * @return array<Use_>
+     */
+    private function getUseStatementNodes(): array
+    {
+        $uniqueClasses = array_unique($this->classesToUse);
+
+        // Converting the string versions of the FQN to a PHP use statement
+        return array_map(
+            static fn (string $classToUse) => new Use_([new UseUse(new Name($classToUse))]),
+            $uniqueClasses,
+        );
+    }
+}
---
+++ src/Bundle/test/src/Migration/CodeGenatorTest.php
@@ -0,0 +1,200 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace App\Migration;
+
+use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\Name;
+use PhpParser\Node\Scalar\String_;
+use PhpParser\Node\Stmt\Echo_;
+use PHPUnit\Framework\TestCase;
+use Sylius\Bundle\GridBundle\Migration\CodeGenerator;
+use Sylius\Bundle\GridBundle\Migration\CodeOutputter;
+
+class CodeGenatorTest extends TestCase
+{
+    private CodeGenerator $codeGenerator;
+
+    private CodeOutputter $codeOutputter;
+
+    public function setup(): void
+    {
+        $this->codeOutputter = new CodeOutputter();
+        $this->codeGenerator = new CodeGenerator($this->codeOutputter);
+    }
+
+    public function testGeneratingAnEmptyFile(): void
+    {
+        $this->assertEquals("<?php\n", $this->codeGenerator->build());
+    }
+
+    /** @dataProvider dataGeneratingImportStatements */
+    public function testGeneratingImportStatements(array $statementsToImport, string $expectedCode): void
+    {
+        foreach ($statementsToImport as $class) {
+            $this->codeGenerator->addUseStatement($class);
+        }
+
+        $this->assertSame($expectedCode, $this->codeGenerator->build());
+    }
+
+    public function dataGeneratingImportStatements(): iterable
+    {
+        yield 'Importing one class' => [
+            ['SomeNamespace\\SomeClass'],
+<<<PHP
+<?php
+use SomeNamespace\\SomeClass;
+PHP,
+        ];
+
+        yield 'Importing the same class multiple times and not get conflicts' => [
+            [
+                'SomeNamespace\\SomeClass',
+                'SomeNamespace\\SomeClass',
+            ],
+<<<PHP
+<?php
+use SomeNamespace\\SomeClass;
+PHP,
+        ];
+    }
+
+    public function testGeneratingNamespace(): void
+    {
+        $this->codeGenerator->setNamespace('Sylius\\SomeNamespace');
+
+        $this->assertSame(
+            <<<PHP
+<?php
+namespace Sylius\\SomeNamespace;
+
+PHP,
+            $this->codeGenerator->build(),
+        );
+    }
+
+    public function testGeneratingAClass(): void
+    {
+        $this->codeGenerator->addClass('TestClass', 'SomeClass', [], []);
+
+        $this->assertSame(
+            <<<PHP
+<?php
+class TestClass extends SomeClass
+{
+}
+PHP,
+            $this->codeGenerator->build(),
+        );
+    }
+
+    public function testGeneratingAStaticFunctionThatReturnsAString(): void
+    {
+        $function = CodeGenerator::createStaticFunction('someFunction', 'Hello World');
+
+        $this->assertSame(
+            <<<PHP
+public static function someFunction() : string
+{
+    return 'Hello World';
+}
+PHP,
+            $this->codeOutputter->prettyPrint([$function]),
+        );
+    }
+
+    public function testGeneratingAFunction(): void
+    {
+        $function = CodeGenerator::createFunction(
+            'greet',
+            [['string', 'name']],
+            [new Echo_([new String_('Hello'), new Variable(new Name('name'))])],
+        );
+
+        $this->assertSame(
+            <<<PHP
+public function greet(string \$name) : void
+{
+    echo 'Hello', \$name;
+}
+PHP,
+            $this->codeOutputter->prettyPrint([$function]),
+        );
+    }
+
+    public function testGettingRelativeClass(): void
+    {
+        $relativeName = $this->codeGenerator->getRelativeClassName('SomeNamespace\\SomeClass');
+
+        $this->assertSame('SomeClass', $relativeName->toString());
+        $this->assertEquals(
+            <<<PHP
+<?php
+use SomeNamespace\\SomeClass;
+PHP,
+            $this->codeGenerator->build(),
+        );
+    }
+
+    /** @dataProvider dataGeneratingAResourceClass */
+    public function testGeneratingAResourceClass(string $resourceClass, string $expectedCode): void
+    {
+        $code = $this->codeGenerator->generateGetResourceClassFunction($resourceClass);
+
+        $this->assertEquals(
+            $expectedCode,
+            $this->codeGenerator->build() . "\n" .
+            $this->codeOutputter->prettyPrint([$code]),
+        );
+    }
+
+    public function dataGeneratingAResourceClass(): iterable
+    {
+        yield 'A resource class that exists' => [
+            self::class,
+<<<PHP
+<?php
+use App\Migration\CodeGenatorTest;
+public function getResourceClass() : string
+{
+    return CodeGenatorTest::class;
+}
+PHP
+        ];
+
+        yield 'A class like that does not exist' => [
+            'SomeNamespace\\SomeClass',
+<<<PHP
+<?php
+
+public function getResourceClass() : string
+{
+    return 'SomeNamespace\\\\SomeClass';
+}
+PHP
+        ];
+
+        yield 'Not class like' => [
+            'If you are using the % syntax, please do not',
+<<<PHP
+<?php
+
+public function getResourceClass() : string
+{
+    return 'If you are using the % syntax, please do not';
+}
+PHP
+        ];
+    }
+}
