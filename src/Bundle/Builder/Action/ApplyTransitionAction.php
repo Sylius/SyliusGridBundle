08148e098977be7cc261d0072ab77ab2e897c7ad
Loïc Frémont <lc.fremont@gmail.com>
2023-06-06 15:29:59 +0200

    [GridBuilder] Apply transition action

---
+++ src/Bundle/Builder/Action/ApplyTransitionAction.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\GridBundle\Builder\Action;
+
+final class ApplyTransitionAction
+{
+    public static function create(string $name, string $route, array $routeParameters = [], array $options = []): ActionInterface
+    {
+        $action = Action::create($name, 'apply_transition');
+
+        $options = array_merge([
+            'link' => [
+                'route' => $route,
+                'parameters' => $routeParameters,
+            ],
+            'transition' => $name,
+        ], $options);
+
+        $action->setOptions($options);
+
+        return $action;
+    }
+}
---
+++ src/Bundle/spec/Builder/Action/ApplyTransitionActionSpec.php
@@ -0,0 +1,72 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace spec\Sylius\Bundle\GridBundle\Builder\Action;
+
+use PhpSpec\ObjectBehavior;
+use Sylius\Bundle\GridBundle\Builder\Action\ActionInterface;
+use Sylius\Bundle\GridBundle\Builder\Action\ApplyTransitionAction;
+
+final class ApplyTransitionActionSpec extends ObjectBehavior
+{
+    function it_is_initializable(): void
+    {
+        $this->shouldHaveType(ApplyTransitionAction::class);
+    }
+
+    function it_builds_apply_transition_actions(): void
+    {
+        $action = $this::create('publish', 'app_book_publish', ['id' => 'resource.id']);
+
+        $action->shouldHaveType(ActionInterface::class);
+        $action->toArray()->shouldReturn([
+            'type' => 'apply_transition',
+            'options' => [
+                'link' => [
+                    'route' => 'app_book_publish',
+                    'parameters' => ['id' => 'resource.id'],
+                ],
+                'transition' => 'publish',
+            ],
+        ]);
+    }
+
+    function it_builds_apply_transition_with_options(): void
+    {
+        $action = $this::create(
+            'publish',
+            'app_book_publish',
+            [
+                'id' => 'resource.id',
+            ],
+            [
+                'class' => 'green',
+                'graph' => 'sylius_book_publishing',
+            ],
+        );
+
+        $action->shouldHaveType(ActionInterface::class);
+        $action->toArray()->shouldReturn([
+            'type' => 'apply_transition',
+            'options' => [
+                'link' => [
+                    'route' => 'app_book_publish',
+                    'parameters' => ['id' => 'resource.id'],
+                ],
+                'transition' => 'publish',
+                'class' => 'green',
+                'graph' => 'sylius_book_publishing',
+            ],
+        ]);
+    }
+}
