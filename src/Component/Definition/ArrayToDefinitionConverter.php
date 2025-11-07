06f0dd77022353f436ca6175abe244f1419a4e68
Francis Hilaire <Prometee@users.noreply.github.com>
2025-11-07 18:12:35 +0100

    Fixing argument types

--- src/Component/Definition/ArrayToDefinitionConverter.php
+++ src/Component/Definition/ArrayToDefinitionConverter.php
@@ -32,13 +32,18 @@ final class ArrayToDefinitionConverter implements ArrayToDefinitionConverterInte
         /** @var array<string, mixed> $driverConfiguration */
         $driverConfiguration = $configuration['driver']['options'] ?? [];
 
+        /** @var string $driverName */
+        $driverName = $configuration['driver']['name'];
+
         $grid = Grid::fromCodeAndDriverConfiguration(
             $code,
-            $configuration['driver']['name'],
+            $driverName,
             $driverConfiguration,
         );
 
-        $grid->setProvider($configuration['provider'] ?? null);
+        /** @var string|callable|null $provider */
+        $provider = $configuration['provider'] ?? null;
+        $grid->setProvider($provider);
 
         if (array_key_exists('sorting', $configuration)) {
             /** @var array<string, string> $sorting */
@@ -53,14 +58,20 @@ final class ArrayToDefinitionConverter implements ArrayToDefinitionConverterInte
         }
 
         foreach ($configuration['fields'] ?? [] as $name => $fieldConfiguration) {
+            /** @var string $name */
+            /** @var array<string, mixed> $fieldConfiguration */
             $grid->addField($this->convertField($name, $fieldConfiguration));
         }
 
         foreach ($configuration['filters'] ?? [] as $name => $filterConfiguration) {
+            /** @var string $name */
+            /** @var array<string, mixed> $filterConfiguration */
             $grid->addFilter($this->convertFilter($name, $filterConfiguration));
         }
 
         foreach ($configuration['actions'] ?? [] as $name => $actionGroupConfiguration) {
+            /** @var string $name */
+            /** @var array<string, mixed> $actionGroupConfiguration */
             $grid->addActionGroup($this->convertActionGroup($name, $actionGroupConfiguration));
         }
 
@@ -74,16 +85,24 @@ final class ArrayToDefinitionConverter implements ArrayToDefinitionConverterInte
      */
     private function convertField(string $name, array $configuration): Field
     {
-        $field = Field::fromNameAndType($name, $configuration['type']);
+        /** @var string $type */
+        $type = $configuration['type'];
+        $field = Field::fromNameAndType($name, $type);
 
         if (array_key_exists('path', $configuration)) {
-            $field->setPath($configuration['path']);
+            /** @var string $path */
+            $path = $configuration['path'];
+            $field->setPath($path);
         }
         if (array_key_exists('label', $configuration)) {
-            $field->setLabel($configuration['label']);
+            /** @var string $label */
+            $label = $configuration['label'];
+            $field->setLabel($label);
         }
         if (array_key_exists('enabled', $configuration)) {
-            $field->setEnabled($configuration['enabled']);
+            /** @var bool $enabled */
+            $enabled = $configuration['enabled'];
+            $field->setEnabled($enabled);
         }
         if (array_key_exists('sortable', $configuration)) {
             $sortable = $configuration['sortable'];
@@ -96,10 +115,13 @@ final class ArrayToDefinitionConverter implements ArrayToDefinitionConverterInte
                 $sortable = null;
             }
 
+            /** @var string|null $sortable */
             $field->setSortable($sortable);
         }
         if (array_key_exists('position', $configuration)) {
-            $field->setPosition($configuration['position']);
+            /** @var int $position */
+            $position = $configuration['position'];
+            $field->setPosition($position);
         }
         if (array_key_exists('options', $configuration)) {
             /** @var array<string, mixed> $options */
@@ -115,19 +137,29 @@ final class ArrayToDefinitionConverter implements ArrayToDefinitionConverterInte
      */
     private function convertFilter(string $name, array $configuration): Filter
     {
-        $filter = Filter::fromNameAndType($name, $configuration['type']);
+        /** @var string $type */
+        $type = $configuration['type'];
+        $filter = Filter::fromNameAndType($name, $type);
 
         if (array_key_exists('label', $configuration)) {
-            $filter->setLabel($configuration['label']);
+            /** @var string|bool|null $label */
+            $label = $configuration['label'];
+            $filter->setLabel($label);
         }
         if (array_key_exists('template', $configuration)) {
-            $filter->setTemplate($configuration['template']);
+            /** @var string $template */
+            $template = $configuration['template'];
+            $filter->setTemplate($template);
         }
         if (array_key_exists('enabled', $configuration)) {
-            $filter->setEnabled($configuration['enabled']);
+            /** @var bool $enabled */
+            $enabled = $configuration['enabled'];
+            $filter->setEnabled($enabled);
         }
         if (array_key_exists('position', $configuration)) {
-            $filter->setPosition($configuration['position']);
+            /** @var int $position */
+            $position = $configuration['position'];
+            $filter->setPosition($position);
         }
         if (array_key_exists('options', $configuration)) {
             /** @var array<string, mixed> $options */
@@ -154,6 +186,8 @@ final class ArrayToDefinitionConverter implements ArrayToDefinitionConverterInte
         $actionGroup = ActionGroup::named($name);
 
         foreach ($configuration as $actionName => $actionConfiguration) {
+            /** @var string $actionName */
+            /** @var array<string, mixed> $actionConfiguration */
             $actionGroup->addAction($this->convertAction($actionName, $actionConfiguration));
         }
 
@@ -165,22 +199,34 @@ final class ArrayToDefinitionConverter implements ArrayToDefinitionConverterInte
      */
     private function convertAction(string $name, array $configuration): Action
     {
-        $action = Action::fromNameAndType($name, $configuration['type']);
+        /** @var string $type */
+        $type = $configuration['type'];
+        $action = Action::fromNameAndType($name, $type);
 
         if (array_key_exists('label', $configuration)) {
-            $action->setLabel($configuration['label']);
+            /** @var string $label */
+            $label = $configuration['label'];
+            $action->setLabel($label);
         }
         if (array_key_exists('template', $configuration)) {
-            $action->setTemplate($configuration['template']);
+            /** @var string $template */
+            $template = $configuration['template'];
+            $action->setTemplate($template);
         }
         if (array_key_exists('icon', $configuration)) {
-            $action->setIcon($configuration['icon']);
+            /** @var string $icon */
+            $icon = $configuration['icon'];
+            $action->setIcon($icon);
         }
         if (array_key_exists('enabled', $configuration)) {
-            $action->setEnabled($configuration['enabled']);
+            /** @var bool $enabled */
+            $enabled = $configuration['enabled'];
+            $action->setEnabled($enabled);
         }
         if (array_key_exists('position', $configuration)) {
-            $action->setPosition($configuration['position']);
+            /** @var int $position */
+            $position = $configuration['position'];
+            $action->setPosition($position);
         }
         if (array_key_exists('options', $configuration)) {
             /** @var array<string, mixed> $options */
