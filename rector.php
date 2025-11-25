644a34b54de1b1c2b85c08c281e96240fcfff83a
Loïc Frémont <lc.fremont@gmail.com>
2025-11-25 11:54:51 +0100

    Apply suggestions from code review

--- rector.php
+++ rector.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\PhpSpecToPHPUnit\Set\MigrationSetList;
 use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromAssignsRector;
 
 return static function (RectorConfig $rectorConfig): void {
@@ -13,6 +12,4 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->ruleWithConfiguration(TypedPropertyFromAssignsRector::class, [
         'inline_public' => false,
     ]);
-
-    $rectorConfig->sets([MigrationSetList::PHPSPEC_TO_PHPUNIT]);
 };
