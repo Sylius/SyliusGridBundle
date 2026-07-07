dcf30db56f2fa643a240dbf4aa5adcf7e4ebb1c8
Loïc Frémont <lc.fremont@gmail.com>
2026-07-07 07:56:47 +0200

    feat(deprecations): Deprecate the legacy grids and the grid events
    # Conflicts:
    #	src/Component/Provider/ArrayGridProvider.php

--- src/Component/Event/GridDefinitionConverterEvent.php
+++ src/Component/Event/GridDefinitionConverterEvent.php
@@ -13,9 +13,13 @@ declare(strict_types=1);
 
 namespace Sylius\Component\Grid\Event;
 
+use Sylius\Component\Grid\Attribute\AsGridMutator;
 use Sylius\Component\Grid\Definition\Grid;
 use Symfony\Contracts\EventDispatcher\Event;
 
+/**
+ * @deprecated use the Grid mutators instead
+ */
 final class GridDefinitionConverterEvent extends Event
 {
     private Grid $grid;
@@ -27,6 +31,8 @@ final class GridDefinitionConverterEvent extends Event
 
     public function getGrid(): Grid
     {
+        \trigger_deprecation('sylius/grid-bundle', '1.16', 'Grid events are deprecated, use the grid mutators with the "%s" attribute instead.', AsGridMutator::class);
+
         return $this->grid;
     }
 }
